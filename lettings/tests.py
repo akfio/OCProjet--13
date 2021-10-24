fc939698c1438c910a30464ba3b27091646585cd
akfio <ambroise.caffiaux@gmail.com>
2021-10-24 20:22:05 +0200

    Add test lettings and profiles

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,19 +1,24 @@
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
+from .models import Letting, Address
 
 
 class TestLettingsView(TestCase):
     client = Client()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.address = Address.objects.create(number=5, street='okok', city='lille',
+                                             state=59, zip_code=5900, country_iso_code='FRA')
+        cls.title = Letting.objects.create(address=cls.address, title="Test")
 
     def test_lettings_index(self):
         response = self.client.get(reverse('lettings:index'))
         expected = "<h1>Lettings</h1>"
         assert expected.encode() in response.content
 
-
-"""
     def test_lettings_letting(self):
-        response = self.client.get(reverse('lettings:letting', kwargs={'letting_id': 1}))
-        assert response.status_code == 200
-"""
+        response = self.client.get(reverse('lettings:letting', kwargs={'letting_id': self.title.id}))
+        expected = "<h1>" + str(self.title) + "</h1>"
+        assert expected.encode() in response.content
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,19 +1,24 @@
 from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
+from .models import Profile
+from django.contrib.auth.models import User
 
 
 class TestLettingsView(TestCase):
     client = Client()
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(username='testuser', password='12345')
+        cls.profile = Profile.objects.create(user=cls.user, favorite_city="Lille")
 
     def test_profiles_index(self):
         response = self.client.get(reverse('profiles:index'))
         expected = "<h1>Profiles</h1>"
         assert expected.encode() in response.content
 
-"""
     def test_lettings_letting(self):
-        response = self.client.get(reverse('profiles:profile', kwargs={'username': 'AirWow'}))
-        assert response.status_code == 200
-"""
-
+        response = self.client.get(reverse('profiles:profile', kwargs={'username': self.user}))
+        expected = "<h1>" + str(self.user) + "</h1>"
+        assert expected.encode() in response.content
