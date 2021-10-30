8972de1488acca1c9342e6201b92a4b9f7213b9c
akfio <ambroise.caffiaux@gmail.com>
2021-10-30 05:38:23 +0200

    modification allowed_hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['oc-lettings-8.herokuapp.com', '*']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
