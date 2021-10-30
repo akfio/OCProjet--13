30bb29e7e4e2fb07f965fb93a09085b2259277e5
akfio <ambroise.caffiaux@gmail.com>
2021-10-30 03:00:28 +0200

    add staticsfiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -117,5 +117,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_on_heroku.settings(locals())
