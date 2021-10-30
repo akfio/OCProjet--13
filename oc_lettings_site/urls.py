97029b193d65500680e40ccb943445cf2a0274f6
akfio <ambroise.caffiaux@gmail.com>
2021-10-30 20:49:42 +0200

    add debug-sentry

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,9 +5,15 @@ from lettings.views import index_home
 from profiles import urls as profiles
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+    return division_by_zero
+
+
 urlpatterns = [
     path('', index_home, name='index'),
     path('lettings/', include(lettings, namespace="lettings")),
     path('profiles/', include(profiles, namespace='profiles')),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
