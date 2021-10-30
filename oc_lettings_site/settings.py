66668ab9b4eae990e39eea6d2d94b32f67cbaa21
akfio <ambroise.caffiaux@gmail.com>
2021-10-30 05:54:02 +0200

    modification allowed_hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['oc-lettings-8.herokuapp.com', '127.0.0.1:8000']
 
 # Application definition
 
